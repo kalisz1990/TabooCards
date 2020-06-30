d8f392b9d041b8fc19e5f5dc44582038ec58b135
Pawel Kaliszewski <kaliszewskipawel@o2.pl>
2020-06-30 12:30:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/taboocards/spash_screen/SplashScreen.kt
@@ -0,0 +1,15 @@
+package com.example.taboocards.spash_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taboocards.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+
+
+}
